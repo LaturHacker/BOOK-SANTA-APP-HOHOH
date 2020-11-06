4f441fa682a475e199bdb6e28a39352aa53bfefa
Narendra Waghmare <57708967+LaturHacker@users.noreply.github.com>
2020-11-06 16:58:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+/*import firebase from 'firebase';
+
+require('@firebase/firestore')
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCWaopHU4isIyKNliP2Nk5MVzwvKUdDlPs",
+  authDomain: "booksantaapp-616f6.firebaseapp.com",
+  databaseURL: "https://booksantaapp-616f6.firebaseio.com",
+  projectId: "booksantaapp-616f6",
+  storageBucket: "booksantaapp-616f6.appspot.com",
+  messagingSenderId: "445980364275",
+  appId: "1:445980364275:web:32cc8cdfdd7bd5bdd91686"
+};
+
+  firebase.initializeApp(firebaseConfig)
+  export default firebase.firestore();*/
+
+  import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCWaopHU4isIyKNliP2Nk5MVzwvKUdDlPs",
+  authDomain: "booksantaapp-616f6.firebaseapp.com",
+  databaseURL: "https://booksantaapp-616f6.firebaseio.com",
+  projectId: "booksantaapp-616f6",
+  storageBucket: "booksantaapp-616f6.appspot.com",
+  messagingSenderId: "445980364275",
+  appId: "1:445980364275:web:32cc8cdfdd7bd5bdd91686"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
